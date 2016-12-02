8e3339a0b43ff89be8d607d41a4f9ea605457620
Ted Frohlich <ttf10@case.edu>
2016-12-01 20:24:14 -0500

    Update and rename EEGEKG.m to main_new.m

--- main_new.m
+++ main_new.m
@@ -1,10 +1,11 @@
+%% Microcontroller setup
 a = arduino('/dev/cu.usbmodem1421','Uno');
-%%
-clc
-close all
+
+%% Main Section
+close all;  clc
 clearvars -except a
 
-% Set up plots
+% Initialize axes objects
 eegAx = subplot(2,1,1);  hold on
 ekgAx = subplot(2,1,2);  hold on
 title(eegAx, 'EEG')
@@ -12,42 +13,37 @@ title(ekgAx, 'EKG')
 xlabel(ekgAx, 'Time (sec)')
 co = get(groot, 'DefaultAxesColorOrder');
 
-% Instantiate the library
+% Load LSL library
 disp('Loading the library...');
 lib = lsl_loadlib();
 
-% resolve a stream...
+% Resolve EEG stream
 disp('Resolving an EEG stream...');
 result = {};
 while isempty(result)
     result = lsl_resolve_byprop(lib,'type','EEG');
 end
 
-% create a new inlet
+% Open LSL inlet
 disp('Opening an inlet...');
 inlet = lsl_inlet(result{1});
-
 disp('Now receiving data...');
-
-i = 0;
-tic;
+i = 0;  tic;  % initialize index and time
 
 while true
+    % Update signals
     i = i + 1;  t = toc;
-    
     time(i,:) = t;
     eeg (i,:) = inlet.pull_sample();
     ekg (i,:) = readVoltage(a,0);
     
+    % Plot signals
     plot(eegAx, time, eeg(:,1), 'Color',co(1,:))
     plot(eegAx, time, eeg(:,2), 'Color',co(5,:))
     plot(eegAx, time, eeg(:,3), 'Color',co(6,:))
     plot(eegAx, time, eeg(:,4), 'Color',co(7,:))
-    plot(ekgAx, time, ekg, 'Color',co(2,:))
-    
+    plot(ekgAx, time, ekg,      'Color',co(2,:))
     xlim(eegAx, [t-10 t]);
     xlim(ekgAx, [t-10 t]);
-    
     drawnow()
-    
 end
